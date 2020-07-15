24f8e728db82aeeed1c1c0029320020417dcba81
Subhamp7 <subham306952@gmail.com>
2020-07-15 21:45:56 +0530

    adding LR

---
+++ Logistic_Regression/Gender_Logistic_Regression.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 21:42:00 2020
+
+@author: subham
+"""
+
+
+#loading the libraries
+import pandas as pd
+import numpy as np
+
+#loading the dataset
+dataset=pd.read_csv("gender_classification.csv")
