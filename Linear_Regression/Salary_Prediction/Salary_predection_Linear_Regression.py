0915b907290fb85d970cd7436a722d61aca9831e
Subhamp7 <subham306952@gmail.com>
2020-07-15 21:47:36 +0530

    added metric to get accuracy

--- Linear_Regression/Salary_Prediction/Salary_predection_Linear_Regression.py
+++ Linear_Regression/Salary_Prediction/Salary_predection_Linear_Regression.py
@@ -4,6 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import metrics
+import statsmodels.api as sm
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
@@ -22,6 +24,34 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
+#printing the slope and intercept
+print("Intercept:" ,regressor.intercept_)
+print("Slope:",regressor.coef_)
+
+#passing the X_test value to predict 
+pred=regressor.predict(X_test)
+
+# Evaluating Model
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, pred)))
+print("Accuracy:",metrics.r2_score(y_test, pred))
+
+
+X = sm.add_constant(X)
+est = sm.OLS(y, X)
+est2 = est.fit()
+print("summary()\n",est2.summary())
+print("------------------------------------------------------------------\n")
+print("pvalues\n",est2.pvalues)
+print("------------------------------------------------------------------\n")
+print("tvalues\n",est2.tvalues)
+print("------------------------------------------------------------------\n")
+print("rsquared\n",est2.rsquared)
+print("------------------------------------------------------------------\n")
+print("rsquared_adj\n",est2.rsquared_adj)
+
+
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
@@ -37,3 +67,4 @@ plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
