794baf6dd7f483119261cb71f6d04cd8a0fff498
Subhamp7 <subham306952@gmail.com>
2020-08-08 20:24:57 +0530

    updating

--- Classification/Decision_Tree/Wine_Origin_Prediction/Wine_ML.py
+++ Classification/Decision_Tree/Wine_Origin_Prediction/Wine_ML.py
@@ -10,7 +10,10 @@ from os import path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
 
 
 #loading the dataset( Source UCI)
@@ -23,6 +26,27 @@ print("There are {} null values".format(dataset.isnull().sum().sum()))
 X=dataset.iloc[:,1:].values
 Y=dataset.iloc[:,0].values
 
+#scaling the X datasset
+sc=StandardScaler()
+X=sc.fit_transform(X)
+
 #splitting the X and Y to training set and test set
 X_train, X_test, Y_train, Y_test =train_test_split(X, Y, test_size=0.3, random_state=0)
 
+#
+DTC=DecisionTreeClassifier()
+
+DTC.fit(X_train,Y_train)
+
+pred=DTC.predict(X_test)
+
+cm=confusion_matrix(pred,Y_test)
+
+
+print('\nAccuracy: {:.2f}\n'.format(accuracy_score(Y_test, pred)))
+
+print('\nClassification Report\n')
+print(classification_report(Y_test, pred, target_names=['Wine Type 1', 'Wine Type 2', 'Wine Type 3']))
+
+
+plot_tree(DTC, filled=True)
