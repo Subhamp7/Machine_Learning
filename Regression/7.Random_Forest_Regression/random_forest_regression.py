da3ea58341ff221082d789b00f420cad98fe5a2e
Subhamp7 <subham306952@gmail.com>
2020-08-21 01:34:27 +0530

    added plots for visualization

--- Regression/7.Random_Forest_Regression/random_forest_regression.py
+++ Regression/7.Random_Forest_Regression/random_forest_regression.py
@@ -4,6 +4,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics  import r2_score
 
 # Importing the dataset
 # Importing the dataset
@@ -22,18 +25,30 @@ data=data.dropna( how='any')
 X=data.iloc[:,0:1].values
 Y=data.iloc[:,1:2].values
 
-# Training the Random Forest Regression model on the whole dataset
-from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 5, random_state = 0)
-regressor.fit(X, Y)
+#splitting data into train and test sets
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y, test_size=0.3, random_state=0)
 
+# Training the Random Forest Regression model on the whole dataset
+regressor = RandomForestRegressor(n_estimators = 2, random_state = 0)
+regressor.fit(X_train, Y_train)
+
+#accuracy
+print("The accuracy of the model is : ",r2_score(Y_test, regressor.predict(X_test)))
 
 # Visualising the Random Forest Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, Y, color = 'red')
+X_grid = (np.arange(min(X_train), max(X_train), 0.01)).reshape(-1, 1)
+plt.scatter(X_train, Y_train, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
+plt.title('Random Forest Regression Train')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Random Forest Regression results (higher resolution)
+X_grid = (np.arange(min(X_test), max(X_test), 0.01)).reshape(-1, 1)
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Random Forest Regression Test')
+plt.xlabel('Experience')
 plt.ylabel('Salary')
 plt.show()
